8d677d282c27355b9037484c81c8f23cdbb36e98
Matthew Davidson <matthewd0224@gmail.com>
2025-03-02 02:13:03 +0000

    switched to nuxt from vue

---
+++ whereami-ui/app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Globe from "./components/Globe.vue";
+</script>
+
+<template>
+  <ClientOnly>
+    <Globe />
+    <template #fallback> Loading map... </template>
+  </ClientOnly>
+</template>
+
+<style></style>
---
+++ whereami-ui/components/Globe.vue
@@ -0,0 +1,25 @@
+<template>
+  <div ref="globeDiv"></div>
+</template>
+
+<script>
+import Globe from "globe.gl";
+import { ref, onMounted } from "vue";
+
+export default {
+  setup() {
+    const globeDiv = ref("globeDiv");
+
+    onMounted(() => {
+      const myGlobe = Globe();
+      myGlobe(globeDiv).globeImageUrl(
+        "//unpkg.com/three-globe/example/img/earth-night.jpg",
+      );
+    });
+
+    return {
+      globeDiv,
+    };
+  },
+};
+</script>
